d44f7161addec468c78c98548830cc0ae6b967d1
eunho.moon@gmail.com <eunho.moon@gmail.com>
2022-07-11 09:46:06 +0900

    @Qualifier, @Primary

--- src/main/java/study/spring/core/discount/RateDiscountPolicy.java
+++ src/main/java/study/spring/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,16 @@
 package study.spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import study.spring.core.member.Grade;
 import study.spring.core.member.Member;
 
 @Component
+//@Qualifier("mainDiscountPolicy")
+@Primary
+// @Primary : 같은 타입의 빈에 우선순위를 부여
 public class RateDiscountPolicy implements DiscountPolicy {
 
 	private int discountPercent = 10;
--- src/main/java/study/spring/core/order/OrderServiceImpl.java
+++ src/main/java/study/spring/core/order/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package study.spring.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -8,11 +10,19 @@ import study.spring.core.member.Member;
 import study.spring.core.member.MemberRepository;
 
 @Component
-@RequiredArgsConstructor	// final이 붙은 필수값을 지닌 생성자를 만들어준다.(필드 수정/추가에도 용이)
+//@RequiredArgsConstructor	// final이 붙은 필수값을 지닌 생성자를 만들어준다.(필드 수정/추가에도 용이)
 public class OrderServiceImpl implements OrderService {
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
+
+	@Autowired
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//		public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+//		 @Qualifier : 특정 빈에 이름을 지정하고(빈의 이름이 변경되는 것은 아님), 지정된 빈을 사용할 수 있다.
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
